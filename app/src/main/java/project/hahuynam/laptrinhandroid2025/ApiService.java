b02e5ff473b72848c32a0ee44c0bdaecf6c54c2d
Hhuynam <bayernfc229@gmail.com>
2025-01-13 08:52:41 +0700

    Deleted all files

--- app/src/main/java/project/hahuynam/laptrinhandroid2025/ApiService.java
+++
@@ -1,9 +0,0 @@
-package project.hahuynam.laptrinhandroid2025;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ApiService {
-    @GET("android/api.aspx?action=last_id")
-    Call<LastId> getLastId();
-}
